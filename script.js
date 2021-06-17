c48ff47cec2ebf37661a4b50c70d391764fa21ba
Santiago Barrett <84517048+Santiago-zavala-barrett@users.noreply.github.com>
2021-06-17 17:55:36 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let day = 6
+let age = 19
+if (day <= 4 && age > 2 && age < 17) {
+console.log('Time for school!')
+} 
+else if (age > 18 && day <= 4)
+{
+console.log('Time for work!')
+} else {
+console.log('Time to relax for the weekend!')
+}
