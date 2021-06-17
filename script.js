c7d6f77a69087952bdf0e015cc3d2f97edf6b438
Santiago Barrett <84517048+Santiago-zavala-barrett@users.noreply.github.com>
2021-06-17 19:32:40 +0000

    fixed errors

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 let day = 6
-let age = 19
+let age = 18
 if (day <= 4 && age > 2 && age < 17) {
   console.log('Time for school!')
   }
 else if (age > 18 && day <= 4) {
-
-}
   console.log('Time for work!')
 } else {
   console.log('Time to relax for the weekend!')
